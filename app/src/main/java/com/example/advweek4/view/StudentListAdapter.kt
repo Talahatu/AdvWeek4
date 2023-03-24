098c58eda78928de54786351027feecc587331ea
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-03-24 20:08:33 +0700

    core stuff

---
+++ app/src/main/java/com/example/advweek4/view/StudentListAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.advweek4.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.advweek4.R
+import com.example.advweek4.model.Student
+
+class StudentListAdapter(val studenList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
+    class StudentViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.student_list_item, parent, false)
+        return StudentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        holder.view?.let { v ->
+            v.findViewById<TextView>(R.id.txtId).text = studenList[position].id
+            v.findViewById<TextView>(R.id.txtName).text = studenList[position].name
+            v.findViewById<Button>(R.id.btnDetail).setOnClickListener {
+                val action = StudentListFragmentDirections.actionStudentDetail()
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return studenList.size
+    }
+
+    fun updateStudentList(newStudentList: ArrayList<Student>) {
+        studenList.clear()
+        studenList.addAll(newStudentList)
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/example/advweek4/view/StudentListFragment.kt
@@ -0,0 +1,82 @@
+package com.example.advweek4.view
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.advweek4.R
+import com.example.advweek4.model.Student
+import com.example.advweek4.viewmodel.ListViewModel
+
+class StudentListFragment : Fragment() {
+
+    private lateinit var viewModel:ListViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
+
+
+    fun observeViewModel() {
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+            studentListAdapter.updateStudentList(it)
+        })
+
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            view?.let {v ->
+                if(it == true) {
+                    v.findViewById<TextView>(R.id.txtError).visibility = View.VISIBLE
+                } else {
+                    v.findViewById<TextView>(R.id.txtError).visibility = View.GONE
+                }
+            }
+
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            view?.let {v->
+                if(it == true) {
+                    v.findViewById<RecyclerView>(R.id.recView).visibility = View.GONE
+                    v.findViewById<ProgressBar>(R.id.progLoad).visibility = View.VISIBLE
+                } else {
+                    v.findViewById<RecyclerView>(R.id.recView).visibility = View.VISIBLE
+                    v.findViewById<ProgressBar>(R.id.progLoad).visibility = View.GONE
+                }
+            }
+        })
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[ListViewModel::class.java]
+        viewModel.refresh()
+        val v = view.findViewById<RecyclerView>(R.id.recView)
+        v.layoutManager = LinearLayoutManager(context)
+        v.adapter = studentListAdapter
+        observeViewModel()
+//        view.findViewById<SwipeRefreshLayout>(R.id.SwipeRefreshLayout).setOnRefreshListener {
+//            Handler(Looper.getMainLooper()).postDelayed({
+//                viewModel.studentsLD.value= arrayListOf(Student("a","a","A","a","d",))
+//                view.findViewById<SwipeRefreshLayout>(R.id.SwipeRefreshLayout).isRefreshing=false
+//            },2000)
+//        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+
+
+}
